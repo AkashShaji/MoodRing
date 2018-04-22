84e68f8d6821b5d3aef9df438d44baece0d56a2e
Akash Shaji <akashshaji321@gmail.com>
2018-04-22 10:16:38 -0400

    Made camera class

---
+++ src/main/java/com/example/akash/moodring/Camera.java
@@ -0,0 +1,14 @@
+package com.example.akash.moodring;
+/**
+ * Created by akash on 4/22/2018.
+ */
+public  class Camera {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+}
