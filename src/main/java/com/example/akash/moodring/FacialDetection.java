ff8b3c74ae932c0490330ea82abbdc1699591d55
Akash Shaji <akashshaji321@gmail.com>
2018-04-22 10:01:15 -0400

    initial commit

---
+++ src/main/java/com/example/akash/moodring/FacialDetection.java
@@ -0,0 +1,28 @@
+package com.example.akash.moodring;
+
+/**
+ * Created by akash on 4/22/2018.
+ */
+
+public class FacialDetection {
+    final String fileName = "network.csv";
+
+    int [][]data;
+
+    FacialDetection(){
+        DataParser();
+    }
+
+    void DataParser(){
+        //TODO things with file
+    }
+
+    int getEmotion(int[][] image){
+        //TODO calculate emotion and return it
+        //TODO create system for returning emotions
+        return 0;
+    }
+
+
+
+}
